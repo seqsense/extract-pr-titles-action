de2d86710fbd96bf74e67e164edb0e0428d33913
Atsushi Watanabe <atsushi.w@ieee.org>
2021-06-03 11:14:36 +0900

    Initial implementation

---
+++ README.md
@@ -0,0 +1,22 @@
+# extract-pr-titles-action
+
+GitHub Action to extract PR titles between the given two git refs.
+Wrapper of https://github.com/kt3k/extract-pr-titles
+
+```yaml
+jobs:
+  example:
+    steps:
+      - uses: actions/checkout@v2
+        with:
+          ref: refs/remotes/origin/main
+          fetch-depth: 0
+      - uses: seqsense/extract-pr-titles-action@v0
+        id: titles
+        with:
+          from: origin/release
+          to: origin/main
+          format: "- {title} #{number}"
+          reverse: true
+      - run: echo ${{ steps.titles.outputs.titles }}
+```
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import * as core from '@actions/core'
+import extractPRTitles from 'extract-pr-titles'
+import simpleGit from 'simple-git'
+
+const git = simpleGit(process.cwd())
+
+const logOpts = {
+  from: core.getInput('from'),
+  to: core.getInput('to'),
+  symmetric: false,
+}
+
+const extractOpts = {
+  format: core.getInput('format'),
+  reverse: core.getInput('reverse') == 'true',
+}
+
+const logHandler = (err, result) => {
+  if (err) {
+    core.setFailed(err)
+    return
+  }
+  core.setOutput('titles', extractPRTitles(result.all, extractOpts).join('\n'))
+}
+
+git.log(logOpts, logHandler)
